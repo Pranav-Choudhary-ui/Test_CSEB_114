2583ae60cfba5382902db731494ab60ce9360748
Pranav-Choudhary-ui <pranavsingh4703@gmail.com>
2024-11-07 12:49:51 +0530

    fsdtest

---
+++ script.js
@@ -0,0 +1,29 @@
+
+const quizData = [
+    {
+        question: "What does MERN stand for?",
+        options: ["MongoDB, Express.js, React.js, Node.js", "MySQL, Express.js, React.js, Node.js", "MongoDB, Ember.js, React.js, Node.js", "MongoDB, Express.js, Angular, Node.js"],
+        correct: 0
+    },
+    {
+        question: "What does MERN stand for?",
+        options: ["MongoDB, Express.js, React.js, Node.js", "MySQL, Express.js, React.js, Node.js", "MongoDB, Ember.js, React.js, Node.js", "MongoDB, Express.js, Angular, Node.js"],
+        correct: 0
+    },
+    {
+        question: "What does MERN stand for?",
+        options: ["MongoDB, Express.js, React.js, Node.js", "MySQL, Express.js, React.js, Node.js", "MongoDB, Ember.js, React.js, Node.js", "MongoDB, Express.js, Angular, Node.js"],
+        correct: 0
+    },
+    {
+        question: "What does MERN stand for?",
+        options: ["MongoDB, Express.js, React.js, Node.js", "MySQL, Express.js, React.js, Node.js", "MongoDB, Ember.js, React.js, Node.js", "MongoDB, Express.js, Angular, Node.js"],
+        correct: 0
+    },
+    {
+        question: "What does MERN stand for?",
+        options: ["MongoDB, Express.js, React.js, Node.js", "MySQL, Express.js, React.js, Node.js", "MongoDB, Ember.js, React.js, Node.js", "MongoDB, Express.js, Angular, Node.js"],
+        correct: 0
+    },
+    
+];
